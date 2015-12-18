07b47cebc1679baf42948fdd51cfc117a2c58d65
Weiming Zhai <wmzhai@gmail.com>
2015-12-18 09:54:41 +0800

    todos

--- client/todos.js
+++ client/todos.js
@@ -1,6 +1,6 @@
-if(Meteor.isClient){
-    // client code goes here
-    Template.todos.helpers({
 
-    });
-}
+Template.todos.helpers({
+    'todo' :function(){
+        return Todos.find({},{sort: {createAt:-1}});
+    }
+});
