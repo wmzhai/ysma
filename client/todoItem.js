05753f2a049a622123a52fa66cbcb13d06454d45
Weiming Zhai <wmzhai@gmail.com>
2015-12-19 14:48:33 +0800

    confirm delete

--- client/todoItem.js
+++ client/todoItem.js
@@ -2,6 +2,9 @@ Template.todoItem.events({
     'click .delete-todo': function(event){
         event.preventDefault();
         var documentId = this._id;
-        Todos.remove({ _id: documentId});
+        var confirm = window.confirm("Delete this task?");
+        if(confirm){
+            Todos.remove({ _id: documentId});
+        }
     }
 });
