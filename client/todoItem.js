453cbac02bebf3d819d66568c319ad558d2c1996
Weiming Zhai <wmzhai@gmail.com>
2015-12-21 09:37:08 +0800

    Editing Tasks

--- client/todoItem.js
+++ client/todoItem.js
@@ -6,5 +6,17 @@ Template.todoItem.events({
         if(confirm){
             Todos.remove({ _id: documentId});
         }
+    },
+    'keyup [name=todoItem]': function(event){
+        var documentId = this._id;
+        var todoItem = $(event.target).val();
+        Todos.update({_id:documentId},{$set:{name: todoItem}});
+        console.log("You just tapped a key on your keyboard.");
+    },
+    'keydown [name=todoItem]': function(){
+        console.log("You're holding down a key on your keyboard.");
+    },
+    'keypress [name=todoItem]': function(){
+        console.log("You just pressed one of the keys on your keyboard.");
     }
 });
